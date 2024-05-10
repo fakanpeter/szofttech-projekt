8024d7978cc845109f4ae77166e4c5f6619f7e04
Pesti Tamás <pesti.tamas@hallgato.ppke.hu>
2024-05-10 21:50:30 +0200

    Restore default frontend code

--- 00-pazmany-labor-2024-master/frontend/src/main.js
+++ 00-pazmany-labor-2024-master/frontend/src/main.js
@@ -1,64 +1,6 @@
-import './index.css'
-import {createRouter, createWebHistory} from 'vue-router'
-import HomePage from './components/HomePage.vue'
-import AllDogs from './components/dogs/AllDogs.vue'
-import Contact from './components/common/Contact.vue'
-import FAQ from './components/common/FAQ.vue'
-import SingleDog from "@/components/dogs/SingleDog.vue";
-import Login from "@/components/auth/Login.vue";
-import Register from "@/components/auth/Register.vue";
-import EditDog from "@/components/dogs/EditDog.vue";
-import axios from "axios";
+import './assets/main.css'
 
-import {createApp} from 'vue';
-import store from './store';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const routes = [
-    {path: '/', component: HomePage},
-    {path: '/dogs', component: AllDogs},
-    {path: '/contact', component: Contact},
-    {path: '/faq', component: FAQ},
-    {path: '/dog/:id', name: 'SingleDog', component: SingleDog},
-    {path: '/edit-dog/:id', name: 'EditDog', component: EditDog, meta: {requiresAuth: true}},
-    {path: '/login', component: Login},
-    {path: '/register', component: Register},
-    {path: '/:pathMatch(.*)*', redirect: '/'}
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
-const loggedIn = false;
-
-router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-        // This route requires auth, check if logged in, if not, redirect to login page.
-        if (!loggedIn) {
-            next({
-                path: '/login',
-                query: {redirect: to.fullPath}
-            })
-        } else {
-            next()
-        }
-    } else {
-        next()
-    }
-})
-
-// axios.interceptors.request.use(
-//     (config) => {
-//         if (store.state.token) {
-//             config.headers.Authorization = `Bearer ${store.state.token}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).mount('#app')
---
+++ frontend/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view class="main-content" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/common/Header.vue'
+import Footer from './components/common/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  created() {
+    const meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, initial-scale=1.0';
+    document.head.appendChild(meta);
+  },
+}
+</script>
+
+<style scoped lang="postcss">
+#app {
+  @apply h-screen flex flex-col;
+}
+
+.main-content {
+  @apply flex-grow;
+}
+</style>
+
+<!--<script setup>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+<!--import TheWelcome from './components/TheWelcome.vue'-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+<!--    </div>-->
+<!--  </header>-->
+
+<!--  <main>-->
+<!--    <TheWelcome />-->
+<!--  </main>-->
+<!--</template>-->
+
+<!--<style scoped lang="postcss">-->
+<!--header {-->
+<!--  line-height: 1.5;-->
+<!--}-->
+
+<!--.logo {-->
+<!--  display: block;-->
+<!--  margin: 0 auto 2rem;-->
+<!--}-->
+
+<!--@media (min-width: 1024px) {-->
+<!--  header {-->
+<!--    display: flex;-->
+<!--    place-items: center;-->
+<!--    padding-right: calc(var(&#45;&#45;section-gap) / 2);-->
+<!--  }-->
+
+<!--  .logo {-->
+<!--    margin: 0 2rem 0 0;-->
+<!--  }-->
+
+<!--  header .wrapper {-->
+<!--    display: flex;-->
+<!--    place-items: flex-start;-->
+<!--    flex-wrap: wrap;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
