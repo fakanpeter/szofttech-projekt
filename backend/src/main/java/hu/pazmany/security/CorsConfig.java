f816b197ed079f85fefdaebc7241acc7ac344fb4
Pesti Tamás <pesti.tamas@hallgato.ppke.hu>
2024-05-14 15:23:59 +0200

    Add cors rules for localhost

--- backend/src/main/java/hu/pazmany/security/CorsConfig.java
+++ backend/src/main/java/hu/pazmany/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://vau-vau.web.app/", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:8080")
+                .allowedOrigins("http://localhost/", "https://localhost/", "http://127.0.0.1/", "https://127.0.0.1/", "https://vau-vau.web.app/", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:8080")
                 .allowedMethods("*");
     }
 }
