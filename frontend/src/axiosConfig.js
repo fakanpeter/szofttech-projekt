1706b8cbc1133b00679ef79738cd9b677ec98303
Pesti Tamás <pesti.tamas@hallgato.ppke.hu>
2024-05-15 22:11:56 +0200

    Make nginx apiURL the default

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,4 @@
 export {default as axios} from 'axios';
 
-export const apiURL = 'http://localhost:8080/api'; // local development
-// export const apiURL = '/api'; // nginx
+// export const apiURL = 'http://localhost:8080/api'; // local development
+export const apiURL = '/api'; // nginx
